b1df19d1d566eece06a850ae1434865586c960e3
Wechuli <wechulipaul@yahoo.com>
2018-12-05 12:40:07 +0300

    sanitising the admin

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from .models import Listing
 
 # Register your models here.
-admin.site.register(Listing)
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor', 'city')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor', 'city')
+    search_fields = ('title', 'address', 'descrption',
+                     'state', 'zipcode', 'price', 'city')
+    list_editable = ('is_published',)
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
